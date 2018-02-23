73a99d248e534dd52b40eae34ec5fdb7a6325873
toddpla <toddpla@gmail.com>
2018-02-23 09:09:51 +0000

    =Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-}
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    // console.log(hash);
+  });
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded: ', decoded);
+var hashedPassword = '$2a$10$aUmfdYRIpM/kwfmknFT3FubGoitNexqsM9RTVc8vXFOpMGKf8cGQK'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+
+// var data = {
+//   id: 10
+// }
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded: ', decoded);
 
 
 // var message = 'I am user numebr 3 this is a longer string.';
